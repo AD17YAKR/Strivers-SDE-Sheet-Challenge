31d301a9a283900f758dc44d33bb16c7dfd6dc4c
Aditya Singh <aditya@merchify.in>
2023-06-26 19:33:29 +0530

    Day 2 : Completed

---
+++ Day 2 -Arrays II/inversionOfArray.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+int cnt = 0;
+
+void merge(long long *arr, int low, int mid, int high)
+{
+    vector<long long> temp;
+    int left = low;
+    int right = mid + 1;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            cnt += mid + 1 - left;
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(long long *arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+long long getInversions(long long *arr, int n)
+{
+    mergeSort(arr, 0, n - 1);
+    return cnt;
+}
